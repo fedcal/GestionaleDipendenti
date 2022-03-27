a2bfef182222b2322bb29f48866d8c4fdafeed3b
fedcal <fedcal01@gmail.com>
2022-03-27 22:43:58 +0200

    add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,22 @@ import './App.css';
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
 import ListaDipendentiComponent from './components/ListaDipendentiComponent';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <HeaderComponent/>
-      <div className="container">
-        <ListaDipendentiComponent/>
-      </div>
-      <FooterComponent/>
+      <Router>
+        
+        <HeaderComponent/>
+        <div className='container'>
+          <Routes>
+            <Route path="/" element={<ListaDipendentiComponent/>}/>
+          </Routes>
+        </div>
+        <FooterComponent/>
+        
+      </Router>
     </div>
   );
 }
